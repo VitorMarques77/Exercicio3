a753daceadfb2b5177316bccc61cda1a11063d1d
Vitor Marques <vitorhalffinger@gmail.com>
2019-12-19 23:51:34 -0300

    Atualizacao dos codigos

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class ImportedProduct extends Product {
+	
+	private Double customFee;
+
+	public ImportedProduct() {
+		
+	}
+	public ImportedProduct(String name, Double price, Double customFee) {
+		super(name,price);
+		this.customFee = customFee;
+	}
+	public Double getCustomFee() {
+		return customFee;
+	}
+	public void setCustomFee(Double customFee) {
+		this.customFee = customFee;
+	}
+	public Double totalPrice() {
+		return price+customFee;
+	}
+	@Override
+	public String priceTag() {
+		return name+" $ "+String.format("%.2f", totalPrice())+String.format(" (Custom fee: $ %.2f)", customFee);
+	}
+}
---
+++ src/entities/UsedProduct.java
@@ -0,0 +1,29 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UsedProduct extends Product {
+	
+	public static final SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+	private Date manufactureDate;
+	
+	public UsedProduct() {
+		
+	}
+	public UsedProduct(String name, Double price, Date manufactureDate) {
+		super(name, price);
+		this.manufactureDate = manufactureDate;
+	}
+	public Date getManufactureDate() {
+		return manufactureDate;
+	}
+	public void setManufactureDate(Date manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
+	@Override
+	public String priceTag() {
+		return name+" (used) $ "+String.format("%.2f", price)+" (Manufacture date: "+sdf1.format(manufactureDate)+")";
+	}
+	
+}
